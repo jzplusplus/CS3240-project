fae71ff5005ff6c678dcaf08a8eecc21d86f16ca
jordankillpack <jordan.killpack@gatech.edu>
2012-11-06 11:35:12 -0500

    Fixed bug in DFA-- the transition table should now have a correct type signature

--- src/util/DFA.java
+++ src/util/DFA.java
@@ -3,10 +3,10 @@ package util;
 import java.util.Map;
 
 public class DFA {
-    private Map<Character, State> transitionTable;
+    private Map<State, Map<Character, State> transitionTable;
     private State currentState;
     private State startState;
-    public DFA(final Map<Character, State> transitionTable, final State startState) {
+    public DFA(final Map<State, Map<Character, State> transitionTable, final State startState) {
         this.transitionTable = transitionTable;
         this.currentState = startState;
         this.startState = startState;
@@ -17,7 +17,7 @@ public class DFA {
      * character.
      */
     public State doTransition(final Character input) {
-        this.currentState = this.transitionTable.get(input);
+        this.currentState = this.transitionTable.get(this.currentState).get(input);
         return this.currentState; 
     }
 
