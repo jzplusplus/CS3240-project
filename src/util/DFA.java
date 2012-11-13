ed715f124901544e9f12aa834dfa0e5e31a44b37
Chris Min <chrismin1202@gatech.edu>
2012-11-12 19:23:13 -0500

    DFA, NFA modified test file added to test NFA to DFA conversion

--- src/util/DFA.java
+++ src/util/DFA.java
@@ -1,36 +1,207 @@
 package util;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class DFA {
-    private Map<State, Map<Character, State>> transitionTable;
-    private State currentState;
-    private State startState;
-    public DFA(final Map<State, Map<Character, State>> transitionTable, final State startState) {
-        this.transitionTable = transitionTable;
-        this.currentState = startState;
-        this.startState = startState;
-    }
 
-    /**
-     * Changes the current state to the next state, based on the input
-     * character.
-     */
-    public State doTransition(final Character input) {
-        this.currentState = this.transitionTable.get(this.currentState).get(input);
-        return this.currentState; 
-    }
+	private Map<State, Map<Character, State>> transitionTable;
+	private State currentState;
+	private State startState;
 
-    public void reset() {
-        this.currentState = this.startState;
-    }
+	public DFA(final Map<State, Map<Character, State>> transitionTable, final State startState) {
+		this.transitionTable = transitionTable;
+		this.currentState = startState;
+		this.startState = startState;
+	}
 
-    public boolean canAccept(String inputString) {
-        this.reset();
-        for (Character inputChar : inputString.toCharArray()) {
-            this.doTransition(inputChar);
-        }
-        return currentState.isAccepting();
-    }
+	/**
+	 * Changes the current state to the next state, based on the input
+	 * character.
+	 */
+	public State doTransition(final Character input) {
+		this.currentState = this.transitionTable.get(this.currentState).get(input);
+		return this.currentState; 
+	}
+
+	public void reset() {
+		this.currentState = this.startState;
+	}
+
+	public boolean canAccept(String inputString) {
+		this.reset();
+		for (Character inputChar : inputString.toCharArray()) {
+			this.doTransition(inputChar);
+		}
+		return currentState.isAccepting();
+	}
+
+	public static class DFABuilder {
+
+
+		private NFA nfa;
+		
+		private State startState;
+		private Map<State, Map<Character, State>> transitionTable;
+		// private Set<State> epsilonTransitionStates;
+
+		public DFABuilder(NFA nfa) {
+			this.nfa = nfa;
+			startState = nfa.getStartState();
+			transitionTable = new HashMap<State, Map<Character, State>>();
+		}
+
+		/**
+	     *      
+		 * @return
+		 */
+		public DFA build() {
+			
+			// initializations
+			Set<Character> characterSet = nfa.getAllowableCharacters();
+			Set<State> initialEpsilonStates = nfa.getEpsilonClosure(startState);
+			Set<State> nfaStates = nfa.getAllStates();
+			if (!nfaStates.contains(startState)) nfaStates.add(startState);
+			
+			boolean accepting = false;
+						
+			// remove epsilon closure
+			Set<IntermediateState> intermediateStates = new HashSet<IntermediateState>();
+			for (State from : nfaStates) {
+				accepting = false;
+				Set<State> epsClosure = nfa.getEpsilonClosure(from);
+				for (State to : epsClosure ) {
+					if (to.isAccepting()) accepting = true; 
+				}
+				intermediateStates.add(new IntermediateState(epsClosure, accepting));
+			}
+						
+			// take care of epsilon closure
+			Set<IntermediateState> dfaStates = new HashSet<IntermediateState>();
+			IntermediateState dfaStartState = new IntermediateState(initialEpsilonStates, containsAcceptingState(initialEpsilonStates));
+			// dfaStartState.setMarker(true);
+			dfaStates.add(dfaStartState);
+			
+			while (containsUnmarkedState(dfaStates)) {
+				IntermediateState is = getUnmarkedState(dfaStates);
+				is.setMarker(true);
+				for (Character input : characterSet) {
+					for (State s : is.getMemberStates()) {
+						Set<State> epsTransitions = getEpsilonClosure(nfa.getTransition(s, input));
+						IntermediateState newIstate = convertToIntermediateState(epsTransitions);
+						is.addTransition(input, newIstate);
+						if (!containsAsMember(dfaStates,epsTransitions)) {
+							dfaStates.add(newIstate);
+						}
+					}
+				}
+			}
+			
+			
+			return new DFA(this.transitionTable, startState);
+		} // end build
+		
+		private IntermediateState convertToIntermediateState(Set<State> nfaStates) {
+			boolean accepting = false;
+			for (State s : nfaStates) {
+				if (s.isAccepting()) accepting = true;
+			}
+			return new IntermediateState(nfaStates,accepting);
+		}
+		
+		/**
+		 * Finds all possible epsilon transitions from a given set of states
+		 * and returns the set of all epsilon transitions
+		 * 
+		 * @param states a set of states
+		 * @return a set of states that are reachable from the given set of states (epsilon transitions)
+		 */
+		private Set<State> getEpsilonClosure(Set<State> states) {
+			Set<State> newStates = new HashSet<State>();
+			// sets do not allow duplicates, so should not be a problem...
+			for (State s : states) {
+				newStates.addAll(nfa.getEpsilonClosure(s));
+			}
+			return (newStates.isEmpty() ? null : newStates);
+		}
+		
+		private boolean containsUnmarkedState(Set<IntermediateState> iStates) {
+			for (IntermediateState is : iStates) {
+				if (is.getMarker()) return true;
+			}
+			return false;
+		}
+		
+		private IntermediateState getUnmarkedState(Set<IntermediateState> iStates) {
+			for (IntermediateState is : iStates) {
+				if (is.getMarker()) return is;
+			}
+			return null;
+		}
+		
+		/**
+		 * Returns true if the set of IntermediateStates contains the given set of States
+		 * 
+		 * @param iStates a Set of IntermediateStates
+		 * @param states a Set of States
+		 * @return true if a member; otherwise, false
+		 */
+		private boolean containsAsMember(Set<IntermediateState> iStates, Set<State> states) {
+			for (IntermediateState iState : iStates) {
+				if (iState.isMember(states)) return true;
+			}
+			return false;
+		}
+		
+		
+		private boolean containsAcceptingState(Set<State> states) {
+			for (State s : states) {
+				if (s.isAccepting()) return true;
+			}
+			return false;
+		}
+		
+		/**
+		 * IntermediateState
+		 *
+		 */
+		private class IntermediateState extends State {
+			
+			Set<State> memberStates;
+			Map<Character, IntermediateState> transitionMap;
+			boolean marker;
+			
+			public IntermediateState(Set<State> memberStates, boolean isAcceptingState) {
+				super(isAcceptingState);
+				this.memberStates = memberStates;
+				transitionMap = new HashMap<Character, IntermediateState>();
+				marker = false;
+			}
+			
+			public Set<State> getMemberStates() {
+				return memberStates;
+			}
+			
+			public boolean isMember(Set<State> memberStates) {
+				return memberStates.equals(this.memberStates);
+			}
+			
+			public void addTransition(Character input, IntermediateState to) {
+				transitionMap.put(input, to);
+			}
+			
+			public boolean getMarker() {
+				return marker;
+			}
+			
+			public void setMarker(boolean marker) {
+				this.marker = marker;
+			}
+		} // end private class IntermediateState
+
+
+	}
 
 }
--- src/util/NFA.java
+++ src/util/NFA.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Queue;
 import java.util.Set;
 
@@ -13,9 +12,9 @@ public class NFA {
     public static final Character EPSILON = null;
 
     // we need two keys: a State and an input Character.
-    private Map<State, Map<Character, Set<State>>> transitionTable = new HashMap<State, Map<Character, Set<State>>>();
+    private Map<State, Map<Character, Set<State>>> transitionTable;
     private State startState;
-    public NFA() { }
+
     public NFA(final Map<State, Map<Character, Set<State>>> transitionTable, final State startState) {
         this.transitionTable = transitionTable;
         this.startState = startState;
@@ -31,6 +30,7 @@ public class NFA {
         return startState;
     }
 
+    // null checking necessary
     public Set<State> getEpsilonClosure(final State state) {
         Queue<State> statesToVisit = new LinkedList<State>();
         statesToVisit.add(state);
@@ -47,113 +47,44 @@ public class NFA {
         return epsilonClosure;
 
     }
-
-    public Set<State> getAcceptStates() {
-        Set<State> acceptStates = new HashSet<State>();
-        for (State state : transitionTable.keySet()) {
-            if (state.isAccepting()) {
-                acceptStates.add(state);
-            }
-        }
-        return acceptStates;
+    
+    public Set<Character> getAllowableCharacters() {
+    	return transitionTable.get(startState).keySet();
+    }
+    
+    public Set<State> getAllStates() {
+    	return transitionTable.keySet();
+    }
+    
+    public Character getEpsilon() {
+    	return EPSILON;
     }
 
-
-
-    public NFA withTransition(State original, Character input, State transition) {
-        if (!transitionTable.containsKey(original)) {
-            transitionTable.put(original, new HashMap<Character, Set<State>>());
-        }
-        if (!transitionTable.get(original).containsKey(input)) {
-            transitionTable.get(original).put(input, new HashSet<State>());
-        }
-
-        transitionTable.get(original).get(input).add(transition);
-        return this;
-    }
-
-    public NFA withTransitions(Map<State, Map<Character, Set<State>>> transitions) {
-        for (State orig : transitions.keySet()) {
-            for (Character input : transitions.get(orig).keySet()) {
-                for (State fin : transitions.get(orig).get(input)) {
-                    this.withTransition(new State(orig), input, new State(fin));
-                }
-            }
-        }
-        return this;
-    }
-
-    @SuppressWarnings("unchecked")
-    public void replace(State oldState, State newState) {
-        Map<State, Map<Character, Set<State>>> clone = 
-            (Map<State, Map<Character, Set<State>>>) ((HashMap<State, Map<Character, Set<State>>>) transitionTable).clone();
-        for (State inState : clone.keySet()) {
-            for (Entry<Character, Set<State>> trans : clone.get(inState).entrySet()) {
-                for (State outState : trans.getValue()) {
-                    if (outState.equals(oldState)) {
-                        transitionTable.get(inState).get(trans.getKey()).remove(oldState);
-                        transitionTable.get(inState).get(trans.getKey()).add(newState);
-                    }
-                }
-            }
-
-            if (inState.equals(oldState)) {
-                transitionTable.put(newState, transitionTable.get(inState));
-                transitionTable.remove(inState);
-            }
-        }
-    }
+    public static class NFABuilder {
         
+        private State startState;
+        private Map<State, Map<Character, Set<State>>> transitionTable = new HashMap<State, Map<Character, Set<State>>>();
+        public NFABuilder() { }
+        public NFABuilder withStartState(State startState) {
+            this.startState = startState;
+            return this;
+        }
 
+        public NFABuilder withTransition(State original, Character input, State transition) {
+            if (!transitionTable.containsKey(original)) {
+                transitionTable.put(original, new HashMap<Character, Set<State>>());
+            }
+            if (!transitionTable.get(original).containsKey(input)) {
+                transitionTable.get(original).put(input, new HashSet<State>());
+            }
 
-    public static NFA union(NFA one, NFA two) {
-        if (two == null) {
-            return one;
+            transitionTable.get(original).get(input).add(transition);
+            return this;
         }
-        if (one == null) {
-            return two;
-        }
-        NFA union = new NFA().withTransitions(one.transitionTable)
-                             .withTransitions(two.transitionTable);
-        union.startState = new State();
-        union.withTransition(union.startState, EPSILON, one.startState);
-        union.withTransition(union.startState, EPSILON, two.startState);
-        return union;
-    }
 
-    public static NFA concatenate(NFA one, NFA two) {
-        if (two == null) {
-            return one;
+        public NFA build() {
+            return new NFA(this.transitionTable, startState);
         }
-        if (one == null) {
-            return two;
-        }
-        NFA concat = new NFA().withTransitions(one.transitionTable)
-                              .withTransitions(two.transitionTable);
-        concat.startState = one.startState;
-        for (State oldAccept : one.getAcceptStates()) {
-            State newState = new State(oldAccept);
-            newState.setAccepting(false);
-            concat.replace(oldAccept, newState);
-            concat.withTransition(newState, EPSILON, two.startState);
-        }
-        return concat;
-    }
 
-    public static NFA kleeneStar(NFA one) {
-        if (one == null) {
-            return null;
-        }
-        NFA star = new NFA().withTransitions(one.transitionTable);
-        star.startState = new State(true);
-        star.withTransition(star.startState, EPSILON, one.startState);
-        for (State accept : one.getAcceptStates()) {
-            star.withTransition(accept, EPSILON, one.startState);
-        }
-        return star;
-    }
-
-    public static NFA kleenePlus(NFA one) {
-        return concatenate(one, kleeneStar(one));
     }
 }
