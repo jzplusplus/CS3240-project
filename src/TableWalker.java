1abe940938ed97012e225b66bcf284cb5aba6762
Jay Zuerndorfer <jzplusplus@gmail.com>
2012-11-13 19:03:05 -0500

    Re-did the TableWalker so that it will use multiple DFAs and find the longest match. Does not yet return the type of a token.

--- src/TableWalker.java
+++ src/TableWalker.java
@@ -1,48 +1,78 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import util.DFA;
+import util.DFA2;
+import util.State;
 
 
 public class TableWalker {
 	
 	private FileReader reader;
+	private List<DFA2> dfaList;
 	
-	public TableWalker(String filepath) throws FileNotFoundException
+	public TableWalker(String filepath, List<DFA2> dfaList) throws FileNotFoundException
 	{
 		reader = new FileReader(filepath);
+		this.dfaList = dfaList;
 	}
 	
-	public String nextToken(DFA dfa) throws IOException, BadTokenException
+	public String nextToken() throws IOException, BadTokenException
 	{
-		String token = "";
-		boolean endOfToken = false;
-		while(!endOfToken)
+		String currentToken = "";
+		String longestValidToken = null;
+		DFA2 validTokenType = null;
+		List<DFA2> possibleTypes = new ArrayList<DFA2>(dfaList);
+		
+		while(true)
 		{
 			int nextInt = reader.read();
-			if(nextInt == -1) endOfToken = true;
-			else
+			if(nextInt == -1) break;
+			
+			char next = (char)nextInt;
+			if( (next == ' ') ||
+				(next == '\t') ||
+				(next == '\n') ||
+				(next == '\r') )
 			{
-				char next = (char)nextInt;
-				if( (next == ' ') ||
-					(next == '\t') ||
-					(next == '\n') ||
-					(next == '\r') )
+				break;
+			}
+			
+			for(int i=0; i<possibleTypes.size(); i++)
+			{
+				DFA2 currentType = possibleTypes.get(i);
+				State state = currentType.doTransition(next);
+				if(state == null)
 				{
-					endOfToken = true;
+					possibleTypes.remove(i);
+					i--;
 				}
-				else{
-					token = token + next;
+				else
+				{
+					currentToken += next;
+					if(state.isAccepting())
+					{
+						reader.mark(255);
+						longestValidToken = currentToken;
+						validTokenType = currentType;
+					}
 				}
 			}
+			
+			if(possibleTypes.size()==0) break;
 		}
 		
-		if(dfa.canAccept(token))
+		reader.reset();
+		
+		if(longestValidToken == null)
 		{
-			return token;
+			throw new BadTokenException(currentToken);
 		}
-		else throw new BadTokenException(token);
+		else
+		{
+			return longestValidToken;
+		}
 	}
 }
